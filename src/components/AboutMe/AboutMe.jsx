17d46fdfeefacdcfb8f3f3f2dea75c3207cd45bf
miguelraad28 <miguelraad2020@gmail.com>
2023-01-13 13:56:09 -0300

    a

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -10,16 +10,16 @@ const AboutMe = () => {
             </div>
             <div className="infoAboutMe">
                 {language === "EN" ?
-                    <><p>I am a youngh Venezuelan living in Argentina and at my 20 years old I am a MERN Stack web
+                    <><p>I am a youngh venezuelan living in Argentina and at my 20 years old I am a MERN Stack web
                         developer and graphic designer. Exited about keep learning and working with what I love that it's the IT
                         area.</p>
-                        <p>At this moment I'm capable to create a Full Stack website, which includes a very nice Front-End and a complete Back-End server managed with Node, Express, MongoDB & Mongoose.</p>
-                        <p>My nexts steps are focused on mobile apps development with React Native and blockchain transfers</p>
+                        <p>At this moment I'm qualified to create a Full Stack website, for the Front-End I usually use ReactJs and the Back-End with NodeJs, Express, MongoDB & Mongoose</p>
+                        <p>My nexts steps are focused on mobile app development with React Native and blockchain transfers</p>
                     </>
                     :
                     <>
                         <p>Soy un joven venezolano viviendo en Argentina y a mis 20 años soy MERN Stack web y diseñador gráfico. Emocionado por seguir aprendiendo y trabajando con lo que amo que es el área de IT.</p>
-                        <p>En este momento soy capaz de crear un sitio web Full Stack, que incluye un muy buen Front-End y un completo servidor Back-End administrado con Node, Express, MongoDB y Mongoose.</p>
+                        <p>En este momento estoy capacitado para crear un sitio web Full Stack, el Front-End lo suelo trabajar en ReactJS y el Back-End con NodeJs, Express, MongoDB & Mongoose</p>
                         <p>Mis próximos pasos se centran en el desarrollo de aplicaciones móviles con React Native y transferencias blockchain</p>
                     </>}
             </div>
--- src/components/Studies/Studies.jsx
+++ src/components/Studies/Studies.jsx
@@ -12,24 +12,24 @@ const Studies = () => {
                 {language === "EN" ?
                     <>
                         <p>
-                            My formal IT education started between 2018 and 2019, I took courses of web development from the Ministry of Education in Buenos Aires, Argentina and discovered the passion that I had for coding.
+                            <strong>2018-2019</strong> I Web development formation courses of the Ministry of Education in Buenos Aires, Argentina and discovered the passion that I had for coding.
                         </p>
                         <p>
-                            I went to a IT oriented high school and learned basics of HTML, CSS, JavaScript & SQL. (Finished 2021)
+                            <strong>2017-2021</strong> IT oriented High School (Web development and SQL)
                         </p>
-                        <p>And after these little knowledges that I was adquiring, i got to the biggest part of my path. March 2022, I started the FULL STACK Developer carreer in <a href="https://www.coderhouse.com/">CoderHouse</a>, went throgh Front-End (HTML, CSS, SCSS, Bootstrap, Git, GitHub, FileZilla, JavaScript, ReactJs) and a little of Back-End(NodeJs, Firebase). In the mid time of the courses I've been studing by my self too, for example, the creation of a complete Back-End from scratch, Admin Panel for ecommerces, upload, update, modify visibility, prices & stock of products, users administration, sales, etc.</p>
-                        <p>And now, January 2023 the beginning of the end starts, Back-End Development in CoderHouse, to polish details and finish expanding my knowledge in the MERN Stack Web Development branch.</p>
+                        <p><strong>2022-2023</strong> Full Stack carreer at <a href="https://www.coderhouse.com/">CoderHouse</a>. Web development, JavaScript & ReactJS finished.</p>
+                        <p>My Back-End habilities were self-taught, but I'm also studying Back-end in CoderHouse to polish details and improve my knowledge.</p>
                     </>
                     :
                     <>
                         <p>
-                            Mi educación formal en IT comenzó entre 2018 y 2019, realicé cursos de desarrollo web del Ministerio de Educación en Buenos Aires, Argentina y descubrí la pasión que tenía por la programación.
+                            <strong>2018-2019</strong> Cursos de formación de desarrollo web del Ministerio de Educación en Buenos Aires, Argentina y descubrí la pasión que tenía por la programación.
                         </p>
                         <p>
-                            Me eduqué en una escuela secundaria orientada en Informática y aprendí los conceptos básicos de HTML, CSS, JavaScript y SQL. (Terminado en 2021)
+                            <strong>2017-2021</strong> Educación secundaria con orientación a informática (Desarrollo web y SQL)
                         </p>
-                        <p>Y luego de esos pequeños conocimientos que fui adquiriendo, llegué a la parte más importante de mi camino como futuro programador. Marzo de 2022, comencé la carrera de Desarrollador FULL STACK en <a href="https://www.coderhouse.com/">CoderHouse</a>, pasé por Front-End (HTML, CSS, SCSS, Bootstrap, Git, GitHub, FileZilla, JavaScript, ReactJs) y un poco de Back-End (NodeJs, Firebase). Entre medio de los cursos también he estado estudiando por mi cuenta, por ejemplo, la creación de un Back-End completo desde cero, Panel de administración de e-commerces, carga, actualización, modificación de visibilidad, precios & stock de productos, administración de usuarios, ventas, etc.</p>
-                        <p>Y ahora, Enero de 2023 comienza el principio del fin, Desarrollo Back-End en CoderHouse, para pulir detalles y terminar de ampliar mis conocimientos en la rama de un MERN Stack Web Developer.</p>
+                        <p><strong>2022-2023</strong> Carrera Full Stack en <a href="https://www.coderhouse.com/">CoderHouse</a>. Desarrollo web, JavaScript & ReactJS terminado.</p>
+                        <p>Mis habilidades de Back-End fueron adquiridas de manera autodidacta, sin embargo ya estoy en cursada de Back-End en CoderHouse para pulir detalles y ampliar mi conocimiento.</p>
                     </>}
                 <h3>{language === "EN" ? "CERTIFICATIONS" : "CERTIFICADOS"}</h3>
                 <div className='certifications'>
