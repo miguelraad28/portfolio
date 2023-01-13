bf709eaf5965afd99829714767e1fd5e56f780a2
miguelraad28 <miguelraad2020@gmail.com>
2023-01-13 15:02:23 -0300

    .

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,14 +13,14 @@ const AboutMe = () => {
                     <><p>I am a youngh venezuelan living in Argentina and at my 20 years old I am a MERN Stack web
                         developer and graphic designer. Exited about keep learning and working with what I love that it's the IT
                         area.</p>
-                        <p>At this moment I'm qualified to create a Full Stack website, for the Front-End I usually use ReactJs and the Back-End with NodeJs, Express, MongoDB & Mongoose</p>
-                        <p>My nexts steps are focused on mobile app development with React Native and blockchain transfers</p>
+                        <p>At this moment I'm qualified to create a Full Stack website, for the Front-End I usually use ReactJs and the Back-End with NodeJs, Express, MongoDB & Mongoose.</p>
+                        <p>My nexts steps are focused on mobile app development with React Native and blockchain transfers.</p>
                     </>
                     :
                     <>
                         <p>Soy un joven venezolano viviendo en Argentina y a mis 20 años soy MERN Stack web y diseñador gráfico. Emocionado por seguir aprendiendo y trabajando con lo que amo que es el área de IT.</p>
-                        <p>En este momento estoy capacitado para crear un sitio web Full Stack, el Front-End lo suelo trabajar en ReactJS y el Back-End con NodeJs, Express, MongoDB & Mongoose</p>
-                        <p>Mis próximos pasos se centran en el desarrollo de aplicaciones móviles con React Native y transferencias blockchain</p>
+                        <p>En este momento estoy capacitado para crear un sitio web Full Stack, el Front-End lo suelo trabajar en ReactJS y el Back-End con NodeJs, Express, MongoDB & Mongoose.</p>
+                        <p>Mis próximos pasos se centran en el desarrollo de aplicaciones móviles con React Native y transferencias blockchain.</p>
                     </>}
             </div>
         </div>
--- src/components/Studies/Studies.jsx
+++ src/components/Studies/Studies.jsx
@@ -15,10 +15,10 @@ const Studies = () => {
                             <strong>2018-2019</strong> I Web development formation courses of the Ministry of Education in Buenos Aires, Argentina and discovered the passion that I had for coding.
                         </p>
                         <p>
-                            <strong>2017-2021</strong> IT oriented High School (Web development and SQL)
+                            <strong>2017-2021</strong> IT oriented High School (Web development and SQL).
                         </p>
-                        <p><strong>2022-2023</strong> Full Stack carreer at <a href="https://www.coderhouse.com/">CoderHouse</a>. Web development, JavaScript & ReactJS finished.</p>
-                        <p>My Back-End habilities were self-taught, but I'm also studying Back-end in CoderHouse to polish details and improve my knowledge.</p>
+                        <p><strong>2022-2023</strong> Full Stack carreer at <a href="https://www.coderhouse.com/">CoderHouse</a>. Front-End web development, JavaScript & ReactJS finished, Back-End currently studying.</p>
+                        <p>My Back-End habilities were self-taught, but I'm polishing details at the carreer.</p>
                     </>
                     :
                     <>
@@ -26,10 +26,10 @@ const Studies = () => {
                             <strong>2018-2019</strong> Cursos de formación de desarrollo web del Ministerio de Educación en Buenos Aires, Argentina y descubrí la pasión que tenía por la programación.
                         </p>
                         <p>
-                            <strong>2017-2021</strong> Educación secundaria con orientación a informática (Desarrollo web y SQL)
+                            <strong>2017-2021</strong> Educación secundaria con orientación a informática (Desarrollo web y SQL).
                         </p>
-                        <p><strong>2022-2023</strong> Carrera Full Stack en <a href="https://www.coderhouse.com/">CoderHouse</a>. Desarrollo web, JavaScript & ReactJS terminado.</p>
-                        <p>Mis habilidades de Back-End fueron adquiridas de manera autodidacta, sin embargo ya estoy en cursada de Back-End en CoderHouse para pulir detalles y ampliar mi conocimiento.</p>
+                        <p><strong>2022-2023</strong> Carrera Full Stack en <a href="https://www.coderhouse.com/">CoderHouse</a>. Desarrollo web Front-End, JavaScript & ReactJS terminado, estudiando actualmente Back-End.</p>
+                        <p>Mis habilidades de Back-End fueron adquiridas de manera autodidacta, sin embargo estoy puliendo detalles en la carrera.</p>
                     </>}
                 <h3>{language === "EN" ? "CERTIFICATIONS" : "CERTIFICADOS"}</h3>
                 <div className='certifications'>
