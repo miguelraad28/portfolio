3a4bd589ead8f47f15f37f0e3b6248c3bced1c73
miguelraad28 <miguelraad2020@gmail.com>
2023-07-27 16:53:32 -0300

    mern => full

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                 <div className='bg-img'></div>
                 <div className="mainContent">
                     <p className={language === "EN" ? "line-1 anim-typewriter-EN" : "line-1 anim-typewriter-ES"}>{language === "EN" ? "Hi, I'm Miguel Raad" : "Hola, soy Miguel Raad"}</p>
-                    <h1 className="line-2 anim-typewriter2">MERN Stack Developer.</h1>
+                    <h1 className="line-2 anim-typewriter2">Full Stack Developer.</h1>
                     <div className="navbarIcons">
                         <a className="pop1 popIcon" target="_blank" href="mailto:miguelraad2020@gmail.com"><i
                             className="bi bi-envelope-fill"></i></a>
